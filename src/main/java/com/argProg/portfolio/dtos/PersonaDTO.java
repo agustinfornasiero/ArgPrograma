d4471baf9ebcfcd7562559919ffe97980ca1600a
agustinfornasiero <agustin_fornasiero@hotmail.com>
2023-05-06 21:46:22 -0300

    Added methods in Persona Controller

---
+++ src/main/java/com/argProg/portfolio/dtos/PersonaDTO.java
@@ -0,0 +1,81 @@
+package com.argProg.portfolio.dtos;
+
+public class PersonaDTO {
+    private String nombre;
+
+    private String apellido;
+
+    private String sobreMi;
+
+    private String email;
+
+    private String telefono;
+
+    private String direccion;
+
+    
+    public PersonaDTO(String nombre, String apellido, String sobreMi, String email, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sobreMi = sobreMi;
+        this.email = email;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public PersonaDTO(){
+
+    }
+
+    //#region Getters and Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getSobreMi() {
+        return sobreMi;
+    }
+
+    public void setSobreMi(String sobreMi) {
+        this.sobreMi = sobreMi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    //#endregion Getters and Setters
+
+}
--- src/main/java/com/argProg/portfolio/services/PersonaService.java
+++ src/main/java/com/argProg/portfolio/services/PersonaService.java
@@ -1,10 +1,12 @@
 package com.argProg.portfolio.services;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.argProg.portfolio.dtos.PersonaDTO;
 import com.argProg.portfolio.model.Persona;
 import com.argProg.portfolio.repositories.IPersonaRepository;
 
@@ -21,5 +23,27 @@ public class PersonaService {
     //Save a new Persona in DB
     public Persona savePersona(Persona per) {
         return _personaRepository.save(per);
+    }
+
+    //Get a Persona by id
+    public Optional<Persona> getById(Long id){
+        return _personaRepository.findById(id);
+    }
+
+    //Update a Persona
+    public Persona updatePersona(PersonaDTO perDto, Long id){
+        Persona  per = _personaRepository.findById(id).get();
+
+        per.setNombre(perDto.getNombre());
+        per.setApellido(perDto.getApellido());
+        per.setDireccion(perDto.getApellido());
+        per.setEmail(perDto.getEmail());
+        per.setDireccion(perDto.getDireccion());
+        per.setTelefono(perDto.getTelefono());
+        per.setSobreMi(perDto.getSobreMi());
+
+        _personaRepository.save(per);
+        return per;
+
     }
 }
